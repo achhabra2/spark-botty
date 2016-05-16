88fee2908edbc2cdef3767f964b4d16a24b8808f
Aman Chhabra <aman@Amandeeps-MacBook-Air.local>
2016-05-15 22:53:27 -0700

    Edited config.js to remove keys and set to environment variables

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  token: 'MTExNDU2ZTYtMWE2Mi00NzQ3LTk0N2EtYTg3NjFjZDM3OTdhMWQ4ZjhjYmEtNzg5',
-  monitorRoom: 'Y2lzY29zcGFyazovL3VzL1JPT00vOWZiOWU1YjQtZWIyMC0zMzc3LWJiM2MtYzRjYjNmODIxYThi',
-  webhookUrl: 'http://a79195b7.ngrok.io'
-  //any thing else you need to reference in the config such as roomId etc
+  // API Token, Room to be monitored by the bot, and webhook URL
+  // Are now defined in the environment variables
+  token: process.env.TOKEN,
+  monitorRoom: process.env.MONITORROOM,
+  webhookUrl: process.env.WEBHOOKURL,
 };
