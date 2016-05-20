d14ad15eb0eb4e52089df5b54fef877f59f1022e
Aman Chhabra <aman.chhabra1@gmail.com>
2016-05-20 12:22:39 -0700

    Removed Monitor Room as it isn't used anymore

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
   token: process.env.NODE_TOKEN,
-  monitorRoom: process.env.NODE_ROOM,
   webhookUrl: process.env.NODE_WEBHOOK,
   //any thing else you need to reference in the config such as roomId etc
 };
